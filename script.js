2ac09f4f2905546239e1b1ae840f6f4ad317ec31
kalilakorin <kalila.korin@gmail.com>
2024-10-26 14:42:58 -0600

    Attempt to find projection of map

--- script.js
+++ script.js
@@ -10,49 +10,76 @@ var map = new mapboxgl.Map({
 
 map.addControl(
   new mapboxgl.AttributionControl({
-    customAttribution: "Icons from www.flaticon.com licensed by CC 3.0"
+    customAttribution: "Icons from www.flaticon.com licensed by CC 3.0",
   })
 );
 
 map.addControl(new mapboxgl.NavigationControl());
+// const projection2 = map.getProjection;
+// const projection2 = map.getProjection();
+// const projection2 = map.getProjection().name;
+// const projection2 = map.getProjection.name;
+// map.setProjection('albers');
+// console.log("Start projection: " + map.getProjection.name);
 
 map.on("load", function () {
   console.log("Map load function...");
-
-  // stamen basemap
-  //   map.addLayer({
-  //     'id': 'stamen-basemap',
-  //     'type': 'raster',
-  //     'source': {
-  //       'type': 'raster',
-  //       'tiles': [
-  //         //'https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg'
-  //         'https://maps.stamen.com/watercolor/#{z}/{x}/{y}'
-  //       ],
-  //       'tileSize': 256
-  //     }
-  //   }, 'denver-food-stores');
-
-  // open street layer
-  // map.addLayer(
-  //   {
-  //     id: "openstreetmap-basemap",
-  //     type: "raster",
-  //     source: {
-  //       type: "raster",
-  //       tiles: ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
-  //       tileSize: 256,
-  //       attribution:
-  //         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  //     },
-  //   },
-  //   "denver-buildings",
-  //   "denver-food-stores"
-  // );
+  // const projection = map.getProjection;
+  // const projection = map.getProjection();
+  // const projection = map.getProjection().name;
+  // const projection = map.getProjection.name;
+  map.setProjection('albers');
+  console.log("Start projection: " + map.getProjection());
 
   // create a popup, but don't add it to the map yet
   const popup = new mapboxgl.Popup({
-    closeOnClick: false
+    closeOnClick: false,
+  });
+
+  // a point to see what the coordinates are for a feature
+  const geojson = {
+    type: "FeatureCollection",
+    features: [
+      {
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: [-104.88799, 39.78023],
+        },
+      },
+    ],
+  };
+
+  map.addSource("point", {
+    type: "geojson",
+    data: geojson,
+  });
+
+  // open street layer
+  map.addLayer(
+    {
+      id: "openstreetmap-basemap",
+      type: "raster",
+      source: {
+        type: "raster",
+        tiles: ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
+        tileSize: 256,
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      },
+    },
+    "denver-buildings",
+    "denver-food-stores"
+  );
+
+  map.addLayer({
+    id: "point",
+    type: "circle",
+    source: "point",
+    paint: {
+      "circle-radius": 10,
+      "circle-color": "#F84C4C", // red color
+    },
   });
 
   map.on("click", "denver-buildings", function (e) {
@@ -60,11 +87,12 @@ map.on("load", function () {
     const type = e.features[0].properties.type;
     // const coordinates = e.features[0].geometry.coordinates.slice();
     // console.log("coordinates\n" + coordinates);
-    console.log("lng lat\n" + e.lngLat);
-
+    console.log("lng lat: " + e.lngLat);
+    console.log("lat: " + e.lngLat.lat + " lang: " + e.lngLat.lng);
 
     // popup = new mapboxgl.Popup()
-    popup.setLngLat(e.lngLat)
+    popup
+      .setLngLat(e.lngLat)
       .setHTML(id + ": " + type)
       .addTo(map);
     // new mapboxgl.Popup()
@@ -76,15 +104,19 @@ map.on("load", function () {
     //   map.getCanvas().style.cursor= '';
     //   popup.remove();
     // });
+
+    //set the geojason point
+    geojson.features[0].geometry.coordinates = [e.lngLat.lng, e.lngLat.lat];
+    map.getSource("point").setData(geojson);
   });
 
-  map.on('mouseenter', 'denver-buildings', function (e) {
-    map.getCanvas().style.cursor = 'crosshair';
+  map.on("mouseenter", "denver-buildings", function (e) {
+    map.getCanvas().style.cursor = "crosshair";
   });
 
-  map.on('mouseleave', 'denver-buildings', function (e) {
-    map.getCanvas().style.cursor= '';
-    popup.remove();
+  map.on("mouseleave", "denver-buildings", function (e) {
+    map.getCanvas().style.cursor = "";
+    // popup.remove();
   });
 });
 
